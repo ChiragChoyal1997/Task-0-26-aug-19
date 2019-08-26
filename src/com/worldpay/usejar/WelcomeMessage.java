d518b19435e0cc453dba5bf9a57e5aedf9d75cea
Chirag <Chirag@Chirag-PC>
2019-08-26 16:50:01 +0530

    My first commit.

---
+++ src/com/worldpay/usejar/WelcomeMessage.java
@@ -0,0 +1,9 @@
+package com.worldpay.usejar;
+import com.worldpay.message.HelloNewBee;
+
+public class WelcomeMessage {
+	public static void main(String args[]) {
+		HelloNewBee h = new HelloNewBee();
+		h.displayMessage();
+	}
+}
